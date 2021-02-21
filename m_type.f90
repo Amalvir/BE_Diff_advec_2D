ce9e086e4a8df909ad80eeb876d881493c9865d9
Amalvir <samalonsovirissel@gmail.com>
2021-02-21 10:58:09 +0100

    Ajout de commentaires.

--- m_type.f90
+++ m_type.f90
@@ -2,11 +2,20 @@ module m_type
 implicit none
 
 type phys
+        ! Données physiques du problème : L Cote du carrée ; 
+        ! D Coeff de diffusion ; C0 Concentration initiale face nord ; 
+        ! C1 Concentration initiale face sud ; alph Paramètre alpha
+
         real :: L,D,C0,C1,alph
 end type phys
 
 type maillage
-        ! Nx,Ny nombre de noeuds le long de x et y
+        ! Données du maillage : dx Delta x ; 
+        ! Nx,Ny Nombre de noeuds le long de x et y ; 
+        ! dyn Delta y entre noeuds ; dyv Delta y entre centres cellules ; 
+        ! xn, yn Matrices taille nx x ny Abscisses et ordonnées des noeuds des volumes. 
+        ! On aurait pu utiliser des vecteurs mais le programme aurait été moins flexible. 
+
         real :: dx
         integer :: Nx,Ny
         real, dimension(:),allocatable :: dyn,dyv
